59b5584a733ba68f5ab70198ddd20405a0562d19
Ben Webb <ben@salilab.org>
2017-03-22 13:15:24 -0700

    Fix test script so that it actually runs.

--- test/test.py
+++ test/test.py
@@ -6,12 +6,13 @@ import sys
 import subprocess
 import glob
 
-
+TOPDIR = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 
 class Tests(unittest.TestCase):
-    def run_imp_script(self, script_name):
+    def test_basic(self):
         """Run IMP modeling"""
+        os.chdir(TOPDIR)
         p = subprocess.check_call(["python", "modeling.py", "--test"])
 
 if __name__ == '__main__':
-unittest.main()
+    unittest.main()
